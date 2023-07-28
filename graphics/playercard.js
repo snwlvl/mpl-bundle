bcd027c7b2e75c465aae3a121fee895e90eb80ca
snwlvl <ronsnwlvl@gmail.com>
2023-07-29 00:52:59 +0530

    replay fix

--- graphics/playercard.js
+++ graphics/playercard.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let isVideoPlaying = false;
+
 nodecg.listenFor('showPlayercard', (newVal) => {
     const container = document.getElementById('container');
     const playerName = document.getElementById('playerName');
@@ -10,59 +12,40 @@ nodecg.listenFor('showPlayercard', (newVal) => {
     teamName.innerHTML = newVal.teamName;
 
     container.style.opacity = 0;
-    fadeIn(container, playerName, teamName, vid);
-
-    // Play the video
-    vid.autoplay = true;
-    vid.controls = false;
-    vid.play();
-});
-
-function fadeIn(container, playerName, teamName, vid) {
-    let op = 0;
     container.style.display = 'block';
     playerName.style.display = 'block';
     teamName.style.display = 'block';
+    vid.style.opacity = 0;
 
-    const timer = setInterval(function() {
-        if (op >= 1) {
-            clearInterval(timer);
-            setTimeout(function() {
-                fadeOut(container, playerName, teamName, vid);
-            }, 10000);
-        }
-        container.style.opacity = op;
-        container.style.filter = 'alpha(opacity=' + op * 100 + ')';
-        playerName.style.opacity = op;
-        playerName.style.filter = 'alpha(opacity=' + op * 100 + ')';
-        teamName.style.opacity = op;
-        teamName.style.filter = 'alpha(opacity=' + op * 100 + ')';
-        vid.style.opacity = op;
-        vid.style.filter = 'alpha(opacity=' + op * 100 + ')';
-        op += op * 0.1 || 0.1;
-    }, 10);
-}
+    if (!isVideoPlaying) {
+        vid.autoplay = true;
+        vid.controls = false;
+        vid.play();
+        isVideoPlaying = true;
 
-function fadeOut(container, playerName, teamName, vid) {
-    let op = 1;
-    const timer = setInterval(function() {
-        if (op <= 0) {
-            clearInterval(timer);
-            container.style.display = 'none';
-            playerName.style.display = 'none';
-            teamName.style.display = 'none';
-            vid.style.display = 'none';
-        }
-        container.style.opacity = op;
-        container.style.filter = 'alpha(opacity=' + op * 100 + ')';
-        playerName.style.opacity = op;
-        playerName.style.filter = 'alpha(opacity=' + op * 100 + ')';
-        teamName.style.opacity = op;
-        teamName.style.filter = 'alpha(opacity=' + op * 100 + ')';
-        vid.style.opacity = op;
-        vid.style.filter = 'alpha(opacity=' + op * 100 + ')';
-        op -= op * 0.1 || 0.1;
-    }, 10);
+        vid.addEventListener('ended', handleVideoEnded);
+    }
+
+    setTimeout(() => {
+        container.style.transition = 'opacity 1s';
+        playerName.style.transition = 'opacity 1s';
+        teamName.style.transition = 'opacity 1s';
+        vid.style.transition = 'opacity 1s';
+
+        container.style.opacity = 1;
+        playerName.style.opacity = 1;
+        teamName.style.opacity = 1;
+        vid.style.opacity = 1;
+    }, 100);
+});
+
+function handleVideoEnded() {
+    isVideoPlaying = false;
+
+    const playerName = document.getElementById('playerName');
+    const teamName = document.getElementById('teamName');
+    playerName.style.opacity = 0;
+    teamName.style.opacity = 0;
 }
 
 const vid = document.getElementById('myVideo');
